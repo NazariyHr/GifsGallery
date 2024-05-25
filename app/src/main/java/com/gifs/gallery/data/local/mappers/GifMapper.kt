857c81a2440c1ec1c021cf3fe73a3db0a86a98dd
Nazar <nazariu11@gmail.com>
2024-05-25 18:28:57 +0300

    code refactor, remove not needed entity

--- app/src/main/java/com/gifs/gallery/data/local/mappers/GifMapper.kt
+++ app/src/main/java/com/gifs/gallery/data/local/mappers/GifMapper.kt
@@ -1,7 +1,6 @@
 package com.gifs.gallery.data.local.mappers
 
 import com.gifs.gallery.data.local.entity.GifEntity
-import com.gifs.gallery.data.local.entity.GifWithSearchKeywords
 import com.gifs.gallery.domain.model.Gif
 
 fun GifEntity.toGif(): Gif {
@@ -10,14 +9,5 @@ fun GifEntity.toGif(): Gif {
         url = url,
         downsizedUrl = downsizedUrl,
         ratio = ratio
-    )
-}
-
-fun GifWithSearchKeywords.toGif(): Gif {
-    return Gif(
-        id = gif.id,
-        url = gif.url,
-        downsizedUrl = gif.downsizedUrl,
-        ratio = gif.ratio
     )
 }
